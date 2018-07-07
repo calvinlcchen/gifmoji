7bd3c30ad6091b179f8b26d96f5b9cf7301160d7
mobeets <mobeets@gmail.com>
2018-07-07 15:00:28 -0400

    adding gif capabilities

--- static/js/gifmoji.js
+++ static/js/gifmoji.js
@@ -1,8 +1,9 @@
 
 var nEmojisToUse = 1000; // max: 900 (ignores values larger)
 var stepSize = 16; // size of rendered emoji
-// var pixelStepSize = 2; // step size over pixels
+var pixelStepSize = 1; // step size over pixels
 
+var widthMult = 0.5;
 var backgroundColor = 50;
 var emojiSize = 32;
 var emojisPerRow = 30;
@@ -10,8 +11,8 @@ var showEmojis = false;
 var emojis; // image of all 32x32 emojis
 var emojiMeanColors; // array of mean color values for all emojis
 
-var timestep = 0;
-var maxTimestep;
+var recording = false;
+var gif;
 
 var canvas;
 var canvasWidth;
@@ -29,56 +30,89 @@ function preload() {
   emojis = loadImage("static/emojis.png");
 }
 
-// function windowResized() {
-//   resizeCanvas(windowWidth, windowHeight);
-// }
+function windowResized() {
+  canvasWidth = widthMult*windowWidth;
+  canvasHeight = Math.round((canvasWidth/2)/1.3333);
+  resizeCanvas(canvasWidth, canvasHeight);
+  // cap.size(canvasWidth/2, canvasHeight);
+}
 
 function setup() {
-  canvasWidth = windowWidth;
+  canvasWidth = widthMult*windowWidth;
   canvasHeight = Math.round((canvasWidth/2)/1.3333);
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.parent('sketch-holder');
   cap = createCapture(VIDEO);
   cap.hide();
-  cap.size(canvas.width/2, canvas.height);
+  cap.size(canvasWidth/2, canvasHeight);
   rectMode(CENTER);
   noStroke();  
   emojiMeanColors = loadEmojiMeanColors(emojis);
   if (nEmojisToUse > emojiMeanColors.length) {
     emojiMeanColors = emojiMeanColors.slice(0, nEmojisToUse);
   }
-  text('loading gifmoji...', cap.width/2, cap.height/2);
+  text('click to start gifmoji, then click again to stop', canvasWidth/4, canvasHeight/2);
+  setupGif();
+}
+
+function setupGif() {
+  // https://gist.github.com/antiboredom/129fd2311dec0046603e
+  // https://github.com/jnordberg/gif.js
+  gif = new GIF({
+    workers: 2,
+    quality: 40,
+    workerScript: "/static/js/gif.worker.js"
+  });
+
+  gif.on('finished', function(blob) {
+    // add created image to html
+    $('#gif-holder').attr("src", URL.createObjectURL(blob));
+    // set width to canvas width
+    $('#gif-holder').css("width", $('#sketch-holder').css("width"));
+    setupGif();
+  });
 }
 
 function saveImage() {
   saveCanvas(canvas, 'gifmoji', 'png');
 }
 
-function mouseClicked() {
-  if (showEmojis) {
-    showEmojis = false;
-    console.log('switch to live mode');
+function mousePressed() {
+  // if (showEmojis) {
+  //   showEmojis = false;
+  //   console.log('switch to live mode');
+  // } else {
+  //   showEmojis = true;
+  //   console.log('switch to still mode');
+  // }
+  recording = !recording;
+  if (!recording) {
+    console.log('rendering');
+    gif.render();
   } else {
-    showEmojis = true;
-    console.log('switch to still mode');
+    console.log('recording');
   }
 }
 
 function draw() {
   if (showEmojis) {
   } else {
-    gifmojify();
+    // gifmojify();
     // image(cap, 0, 0);
     // text('(press right arrow to gifmojify)', cap.width/2, cap.height/2);
   }
   // image(cap, 0, 0);
+  if (recording) { gifmojify(); }
   image(cap, canvas.width/2, 0);
+
+  if (recording && frameCount % 3 == 0) {
+    gif.addFrame(canvas.elt, {delay: 1, copy: true});
+  }
 }
 
 function gifmojify() {
-  // background(backgroundColor);
-  captureToEmojis(cap, stepSize);
-  return;
+  background(backgroundColor);
+  // captureToEmojis(cap, stepSize);
   curCapture = createImage(cap.width, cap.height);
   if (curCapture.width > 0) {
     curCapture.loadPixels();
@@ -91,22 +125,18 @@ function gifmojify() {
   }
 }
 
-function captureToEmojis(cap, stepSize) {
-  // emojiIndex = 1;
-  // var i = 0;
-  // for (var cy = 0; cy < cap.height; cy += stepSize) {
-    // for (var cx = 0; cx < cap.width; cx += stepSize) {
-
+function captureToEmojis(curCap, stepSize) {
   // var cty = random(0, stepSize*Math.floor(cap.height/stepSize));
   // var ctx = random(0, stepSize*Math.floor(cap.width/stepSize));
-  for (var cy = 0; cy < cap.height; cy += stepSize) {
-    for (var cx = 0; cx < cap.width; cx += stepSize) {
-      curPatch = cap.get(cx, cy, stepSize, stepSize);
+  for (var cy = 0; cy < curCap.height; cy += stepSize) {
+    for (var cx = 0; cx < curCap.width; cx += stepSize) {
+      curPatch = curCap.get(cx, cy, stepSize, stepSize);
       if (curPatch.width > 0) {
         curPatch.loadPixels();
+        emojiIndex = findIndexOfClosestMean(avgColorInImage(curPatch),
+            emojiMeanColors);
         updateBlockWithEmoji(cx, cy,
-          findIndexOfClosestMean(avgColorInImage(curPatch),
-            emojiMeanColors), emojis);
+          emojiIndex, emojis);
       }
     }
   }
@@ -128,7 +158,7 @@ function loadEmojiMeanColors(emojis) {
 function avgColorInImage(img){
   img.loadPixels();
   var r = 0, g = 0, b = 0, a = 0;
-  for (var c = 0; c < img.pixels.length-3; c += 4) {
+  for (var c = 0; c < img.pixels.length-3; c += 4*pixelStepSize) {
     r += img.pixels[c];
     g += img.pixels[c+1];
     b += img.pixels[c+2];
@@ -136,7 +166,7 @@ function avgColorInImage(img){
   }
   n = img.pixels.length/4;
   var clr = color(r/n, g/n, b/n, a/n);
-  return clr
+  return clr;
 }
 
 function findIndexOfClosestMean(mu, mus) {
